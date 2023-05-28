0c2d0793bae7ca08ed831e3b79149b800f495e5f
Rodrigo Azevedo <rodrigoams@gmail.com>
2023-05-28 16:36:33 +0200

    new cleaning

--- topologyviewer/data/list2html.lua
+++
@@ -1,8 +0,0 @@
-
-local pipe = assert(io.popen('ls -1 *.data','r'))
-local list = pipe:read('*all')
-pipe:close()
-
-for line in list:gmatch('(%S+)\n') do
-	print(string.format('<option value="./data/%s" > %s </option>',line,line))
-end
